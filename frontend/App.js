753bbe73b4335e09e1abfa89830a43b611bb3c2c
Cristelle <cristelleal@gmail.com>
2026-03-23 14:56:45 +0100

    fix(frontend): reapply missing stack

--- frontend/App.js
+++ frontend/App.js
@@ -13,11 +13,11 @@ function App() {
   return (
     <SocketContext.Provider value={socket}>
       <NavigationContainer>
-        <Navigator initialRouteName="HomeScreen">
-          <Screen name="HomeScreen" component={HomeScreen} />
-          <Screen name="OnlineGameScreen" component={OnlineGameScreen} />
-          <Screen name="VsBotGameScreen" component={VsBotGameScreen} />
-        </Navigator>
+        <Stack.Navigator initialRouteName="HomeScreen">
+          <Stack.Screen name="HomeScreen" component={HomeScreen} />
+          <Stack.Screen name="OnlineGameScreen" component={OnlineGameScreen} />
+          <Stack.Screen name="VsBotGameScreen" component={VsBotGameScreen} />
+        </Stack.Navigator>
       </NavigationContainer>
     </SocketContext.Provider>
   );
